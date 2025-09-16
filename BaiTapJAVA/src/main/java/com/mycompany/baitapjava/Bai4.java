49e83690e3998f56fe5fa1b9d7a68465699f0c39
vu-tm <truongm.vuu@gmail.com>
2025-09-16 14:29:03 +0700

    end

--- BaiTapJAVA/src/main/java/com/mycompany/baitapjava/Bai4.java
+++ BaiTapJAVA/src/main/java/com/mycompany/baitapjava/Bai4.java
@@ -1,5 +1,7 @@
 package com.mycompany.baitapjava;
 
+import java.util.Scanner;
+
 /**
  * - Nhập vào ngày tháng năm
  * - In ra ngày kế tiếp
@@ -7,8 +9,44 @@ package com.mycompany.baitapjava;
  */
 public class Bai4 {
 
+    public boolean isNamNhuan(int n) {
+        return ((n % 400 == 0) || (n % 4 == 0 && n % 100 != 0));
+    }
+
     public void run() {
-        System.out.println("Day la cau 4");
-        // Code bài tập 4 ở đây
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ngay: ");
+        int ngay = sc.nextInt();
+        System.out.print("Thang: ");
+        int thang = sc.nextInt();
+        System.out.print("Nam: ");
+        int nam = sc.nextInt();
+
+        int ngayTrongNam;
+        switch (thang) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 9:
+            case 11:
+                ngayTrongNam = 31;
+                break;
+            case 4:
+            case 6:
+            case 8:
+            case 10:
+            case 12:
+                ngayTrongNam = 30;
+                break;
+            case 2:
+                if (isNamNhuan(nam)) {
+                    ngayTrongNam = 29;
+                } else {
+                    ngayTrongNam = 28;
+                }
+            default:
+                System.out.println("Thang khong hop le! ");
+        }
     }
 }
