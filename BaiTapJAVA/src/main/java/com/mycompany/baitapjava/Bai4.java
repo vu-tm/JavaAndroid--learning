4ee7d82f10f50f751bfa5e8b9552db4ab9b1e780
vu-tm <truongm.vuu@gmail.com>
2025-09-16 14:46:42 +0700

    done

--- BaiTapJAVA/src/main/java/com/mycompany/baitapjava/Bai4.java
+++ BaiTapJAVA/src/main/java/com/mycompany/baitapjava/Bai4.java
@@ -22,21 +22,21 @@ public class Bai4 {
         System.out.print("Nam: ");
         int nam = sc.nextInt();
 
-        int ngayTrongNam;
+        int ngayTrongNam = 0;
         switch (thang) {
             case 1:
             case 3:
             case 5:
             case 7:
-            case 9:
-            case 11:
+            case 8:
+            case 10:
+            case 12:
                 ngayTrongNam = 31;
                 break;
             case 4:
             case 6:
-            case 8:
-            case 10:
-            case 12:
+            case 9:
+            case 11:
                 ngayTrongNam = 30;
                 break;
             case 2:
@@ -45,8 +45,29 @@ public class Bai4 {
                 } else {
                     ngayTrongNam = 28;
                 }
+                break;
             default:
                 System.out.println("Thang khong hop le! ");
         }
+        int ngayNext = 0;
+        int thangNext = 0;
+        int namNext = 0;
+
+        if (ngay == ngayTrongNam) { // cuối tháng
+            ngayNext = 1;
+            thangNext = thang + 1;
+            namNext = nam;
+            if (thangNext > 12) { // cuối năm
+                thangNext = 1;
+                namNext = nam + 1;
+            }
+        } else { // Bình thường
+            ngayNext = ngay + 1;
+            thangNext = thang;
+            namNext = nam;
+        }
+
+        System.out.println("Ngay hien tai: " + ngay + "/" + thang + "/" + nam);
+        System.out.println("Ngay ke tiep: " + ngayNext + "/" + thangNext + "/" + namNext);
     }
 }
